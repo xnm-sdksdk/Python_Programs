db2c4ee3d192fb1651c54d195c9c216ddf075f1b
xnm-sdksdk <n.mendonca@protonmail.com>
2022-11-23 16:44:09 +0000

    search and sort algorithm

---
+++ Search_and_Sort.py
@@ -0,0 +1,12 @@
+
+
+# arr the array we are going to iterate over
+# target param we want to find
+def search(arr, target):
+    for i in range(len(arr)): # iterate over the length of the array
+        if arr[i] == target: # (if element in array iterartion equals target) if we found it then
+            return i # return it
+        
+array = [2,5,8,9,10,16,22]
+target = 10
+print(search(array, target))
